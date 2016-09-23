3bf2d7cfbab0ee61182d768344192476ac2d2b85
John Stallo <john.stallo@live.com>
2016-09-23 11:17:41 -0700

    code cleanup

--- service-a/server.js
+++ service-a/server.js
@@ -3,24 +3,28 @@ var request = require('request');
 var express = require('express');
 var morgan = require('morgan');
 
-var amqp = require('amqp');
-
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(morgan("dev"));
+
+var RABBITMQ_URL = "amqp://rabbitmq";
 
 // api ------------------------------------------------------------
 app.get('/api', function (req, res) {
     res.send("hello world");
 });
 
+app.get('/api/rabbit', function (req, res) {
+    var amqp = require('amqplib/callback_api');
 
-var amqp2 = require('amqplib/callback_api');
-app.get('/api/amqplib', function(req, res) {
-    amqp2.connect('amqp://rabbitmq', function(err, conn) {
-        conn.createChannel(function(err, ch) {
+    amqp.connect(RABBITMQ_URL, function (err, conn) {
+        if (err) {
+            console.log("AMQP Error: %s", err.message);
+              res.status(500).send('Error');
+        }
+        conn.createChannel(function (err, ch) {
             var q = "hello";
-            ch.assertQueue(q, {durable: false});
+            ch.assertQueue(q, { durable: false });
             ch.sendToQueue(q, new Buffer('Hello World'));
             console.log("Sent 'Hello World");
             res.send("sent hello world");
@@ -28,36 +32,13 @@ app.get('/api/amqplib', function(req, res) {
     });
 });
 
-app.get('/api/rabbit', function(req, res) {
-    var connection = amqp.createConnection({host: "rabbitmq"}, {reconnect: false});
-    console.log('Rabbit connection created; waiting for connection... %j', connection);
-
-    connection.on('ready', function() {
-        console.log('Connection ready for use.');
-        connection.queue('hello', {autoDelete: false }, function(q){
-            console.log('Connected to "hello" queue.');
-
-            q.bind('#'); // bind to all messages
-
-            q.on('queueBindOk', function() {
-                var message = 'hello world @ ' + new Date();
-                connection.publish('hello', message);
-
-                console.log('Published message: "' + message + '"');
-                connection.disconnect();
-                res.send("successfully connected to queue");
-            });
-        });
-    });
-});
-
-app.get('/api/patrons', function(req, res) {
-    request('http://patrons/sendrabbit', function (error, response, body) {
+app.get('/api/patrons', function (req, res) {
+    request('http://patrons/patrons', function (error, response, body) {
         res.send(body);
     });
 });
 
-app.post('/api/newpatron', function(req, res) {
+app.post('/api/newpatron', function (req, res) {
     var patron = {
         phone: req.body.patron,
         name: req.body.name,
